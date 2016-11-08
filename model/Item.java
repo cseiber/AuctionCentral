25105912be49987fc3392fc31e8b714dc461b39c
Carl Seiber <CarlSeiber82@gmail.com>
2016-11-07 20:36:44 -0800

    Added data members to the Auction Central Item Class

--- model/Item.java
+++ model/Item.java
@@ -2,6 +2,17 @@ package model;
 
 public class Item {
 	private String myItemName;
+	private String myAuctionName;
+	private String myDonor;
+	private String myCondition;
+	private String mySize;
+	private String myNotes;
+	private String myDescription;
+	private int myUnitQuantity;
+	private int mySellingPrice;
+	private int myItemNumber;
+	private int myMinBid;
+	
 	public Item(String theItemName){
 		myItemName = theItemName;
 	}
