9d7597fbf47ca40e6021f815299e881acb2987c9
awgillett <gillett.aaron@gmail.com>
2016-11-12 20:19:32 -0800

    added more bidder UI

--- view/BidderUI.java
+++ view/BidderUI.java
@@ -8,6 +8,8 @@ import model.Bidder;
 public class BidderUI {
 	
 	static Bidder currentBidder;
+	static Scanner sc = new Scanner(System.in);
+	static int choice;
 	
 	BidderUI()
 	{
@@ -16,10 +18,27 @@ public class BidderUI {
 	}
 	
 	public static void welcomeScreen(Bidder bidder){
-		
-		currentBidder = bidder;
-		System.out.println("Welcome "+ currentBidder.getMyName());
-		System.out.println("in bidder ui");
+				
+		System.out.println("You are now logged in as: " + currentBidder.getMyName());
+		System.out.println("Welcome " + currentBidder.getMyName() + ", what would you like to do?");
+		while (choice != 1 && choice != 2) {
+			System.out.println("");
+			System.out.println("1. View all upcoming auctions");
+			System.out.println("2. Log out and return to main menu.");
+			System.out.println("");
+			System.out.println(">>");
+			choice = sc.nextInt();
+			switch (choice) {
+			case 1:	showAuctions(); break;
+			case 2: System.out.println("You have been logged out of the System"); break;
+			default: System.out.println("Please choose within the range provided");
+			}
+		}
+	}
+	public static void showAuctions() {
+		System.out.println("");
+		System.out.println("This is the list of Auctions");
+
 	}
 
 }
